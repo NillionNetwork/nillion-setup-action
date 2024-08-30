9a6a574c372db53f50eac44c4410007b68146896
Tim Holmes-Mitra <tim.holmesmitra@nillion.com>
2024-08-30 11:19:46 +0100

    feat: expose nillion tool versions

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ import path from "node:path";
 export async function run(): Promise<void> {
   try {
     const version = core.getInput("version");
-    const nilupBinPath = `${process.env.HOME}/.nilup/bin`;
+    const nilupPath = `${process.env.HOME}/.nilup`;
+    const nilCmdPath = `${nilupPath}/bin/nilup`;
     const scriptPath = path.join("/tmp", "install.sh");
 
     await execa({
@@ -13,41 +14,39 @@ export async function run(): Promise<void> {
     })`curl -sSL https://nilup.nilogy.xyz/install.sh -o ${scriptPath}`;
 
     await execa({ stdout: ["pipe", "inherit"] })`bash ${scriptPath}`;
+    await execa({ stdout: ["pipe", "inherit"] })`${nilCmdPath} init`;
+    await execa({ stdout: ["pipe", "inherit"] })`${nilCmdPath} use ${version}`;
 
-    await execa({
+    // Execute binaries directly otherwise commands hang in ci
+    const activeSdk = `${nilupPath}/sdks/${version}`;
+
+    const nadaVer = await execa({
       stdout: ["pipe", "inherit"],
-    })`${nilupBinPath}/nilup use ${version}`;
+    })`${activeSdk}/nada -V`;
 
-    await execa({ stdout: ["pipe", "inherit"] })`${nilupBinPath}/nilup init`;
+    const nillionDevnetVer = await execa({
+      stdout: ["pipe", "inherit"],
+    })`${activeSdk}/nillion-devnet -V`;
 
-    // TODO this causes pids to spawn
-    // const nadaVer = await execa({
-    //   stdout: ["pipe", "inherit"],
-    // })`${nilupBinPath}/nada -V`;
-    //
-    // // const nillionDevnetVer = await execa({
-    // //   stdout: ["pipe", "inherit"],
-    // // })`${nilupBinPath}/nillion-devnet -V`;
-    // //
-    // const nillionVer = await execa({
-    //   stdout: ["pipe", "inherit"],
-    // })`${nilupBinPath}/nillion -V`;
-    //
-    // const nilupVer = await execa({
-    //   stdout: ["pipe", "inherit"],
-    // })`${nilupBinPath}/nilup -V`;
-    //
-    // const pyNadacVer = await execa({
-    //   stdout: ["pipe", "inherit"],
-    // })`${nilupBinPath}/pynadac -V`;
+    const nillionVer = await execa({
+      stdout: ["pipe", "inherit"],
+    })`${activeSdk}/nillion -V`;
 
-    core.addPath(nilupBinPath);
+    const nilupVer = await execa({
+      stdout: ["pipe", "inherit"],
+    })`${activeSdk}/nilup -V`;
 
-    core.setOutput("nada-version", "nadaVer.stdout");
-    core.setOutput("nillion-devnet-version", "nillionDevnetVer.stdout");
-    core.setOutput("nillion-version", "nillionVer.stdout");
-    core.setOutput("nilup-version", "nilupVer.stdout");
-    core.setOutput("pynadac-version", "pyNadacVer.stdout");
+    const pyNadacVer = await execa({
+      stdout: ["pipe", "inherit"],
+    })`${activeSdk}/pynadac -V`;
+
+    core.addPath(`${nilupPath}/bin`);
+
+    core.setOutput("nada-version", nadaVer.stdout);
+    core.setOutput("nillion-devnet-version", nillionDevnetVer.stdout);
+    core.setOutput("nillion-version", nillionVer.stdout);
+    core.setOutput("nilup-version", nilupVer.stdout);
+    core.setOutput("pynadac-version", pyNadacVer.stdout);
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message);
